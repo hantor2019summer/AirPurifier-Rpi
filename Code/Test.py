32157baaecacde03cb6ae2ea12c664e50760b4c4
julie0005 <duliesoures@gmail.com>
2019-07-15 04:04:40 +0100

    for testing

--- Code/Test.py
+++ Code/Test.py
@@ -16,11 +16,12 @@ lcd_rows = 2
 
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-lcd.message('Hello\nworld!')
+lcd.message('ABCDEFGHIJKLMNP\nabcdefghijklmnop!')
 time.sleep(5.0)
 
 lcd.clear()
 
+'''
 text = raw_input("Type Something to be displayed: ")
 
 lcd.message(text)
@@ -32,3 +33,4 @@ lcd.message('Goodbye\nWorld!')
 time.sleep(5.0)
 
 lcd.clear()
+'''
