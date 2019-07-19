c6bc4b49f160a7dc0f024184b9385c60ed5c8423
julie0005 <duliesoures@gmail.com>
2019-07-19 04:36:04 +0100

    add PM sensor

--- Code/Test.py
+++ Code/Test.py
@@ -1,6 +1,6 @@
 import time
 import Adafruit_CharLCD as LCD
-
+from aqi import get_values
 # Raspberry Pi pin setup
 lcd_rs = 25
 lcd_en = 24
@@ -13,6 +13,8 @@ lcd_backlight = 2
 # Define LCD column and row size for 16x2 LCD.
 lcd_columns = 16
 lcd_rows = 2
+
+print(get_values())
 
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
