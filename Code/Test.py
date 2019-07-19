d80280b4a9c0cff121dc9f6dffd3211ffb646d7e
julie0005 <duliesoures@gmail.com>
2019-07-19 05:01:43 +0100

    make roop in Test.py and import aqi.get_values

--- Code/Test.py
+++ Code/Test.py
@@ -1,6 +1,7 @@
 import time
 import Adafruit_CharLCD as LCD
 from aqi import get_values
+
 # Raspberry Pi pin setup
 lcd_rs = 25
 lcd_en = 24
@@ -14,14 +15,15 @@ lcd_backlight = 2
 lcd_columns = 16
 lcd_rows = 2
 
-print(get_values())
-
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
-lcd.message('ABCDEFGHIJKLMNP\nabcdefghijklmnop!')
-time.sleep(5.0)
+while True : 
+    lcd.clear()
 
-lcd.clear()
+    value_string = get_values()
+    lcd.messgae("UltraFine Duse : "+str(value_string[0])+"\nFine Dust : "+str(value_string[1]))
+
+    time.sleep(5.0)
 
 '''
 text = raw_input("Type Something to be displayed: ")
